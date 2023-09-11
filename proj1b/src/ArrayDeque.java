14cbb6f06ab77a1153d574f1a0f5278f0aeb3125
wanglifei <emmawa@foxmail.com>
2023-09-11 22:20:06 +0800

    completed proj1b

---
+++ proj1b/src/ArrayDeque.java
@@ -0,0 +1,155 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque<T> implements Deque<T> {
+    private int size;
+    private int nextFirst; // if =4, Private field 'nextFirst' is assigned but never accessed
+    private int nextLast;
+    private T[] items;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+//        items = (T[]) new T<>[8];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> ad = new ArrayDeque<>();
+        ad.addLast(2);
+        ad.addLast(3);
+        ad.addFirst(1);
+        ad.addLast(4);
+        ad.addLast(5);
+        ad.addFirst(0);
+        ad.addLast(6);
+        ad.addLast(7);
+        ad.addLast(8);
+    }
+
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        for(int i = 0; i < size; i++){ // 循环条件：i小于ArrayDeque的size
+            if(nextFirst + 1 + i < size){ // 如果ArrayDeque的前空位+1即第1位+i小于ArrayDeque的size（最大size-1位）
+                a[nextFirst + 1 + i] = items[nextFirst + 1 + i]; // 新组a相同位置赋item相同值
+            }
+            else{
+                a[nextFirst + 1 + i] = items[nextFirst + 1 + i - size]; // 否则继续赋前面的值
+            }
+        }
+        nextLast = nextFirst + size + 1;
+        items = a;
+    }
+
+    private void resizeDown(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        for(int i = 0; i < size; i++){
+            if(nextFirst + 1 + i < size){
+                a[i] = items[nextFirst + 1 + i];
+            }
+            else{
+                a[i] = items[nextFirst + 1 + i - size]; // 否则继续赋前面的值
+            }
+        }
+        nextFirst = capacity - 1;
+        nextLast = size;
+        items = a;
+    }
+    @Override
+    public void addFirst(T x) {
+        if(size == items.length){
+            resize(size * 2);
+        }
+        if(nextFirst < 0){
+            nextFirst = size - 1;
+        }
+        items[nextFirst] = x;
+        nextFirst -= 1;
+        size += 1;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if(size == items.length){
+            resize(size * 2);
+        }
+        if(nextLast == items.length){
+            nextLast = 0;
+        }
+        items[nextLast] = x;
+        nextLast += 1;
+        size += 1;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < size; i ++) {
+            if(nextFirst + 1 + i < items.length){
+                returnList.add(items[nextFirst + 1 + i]);
+            }
+            else{
+                returnList.add(items[nextFirst + 1 + i - size]);
+            }
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        if (items.length > 16){
+            if ((float)size/ items.length < 0.5) {
+                resizeDown(items.length / 2 + 1);
+            }
+        }
+        T a = items[0];
+        items[0] = null;
+        size -= 1;
+        return a;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        if (items.length > 16){
+            if (size/ items.length < 0.5) {
+                resizeDown(items.length / 2 + 1);
+            }
+        }
+        T b = items[size - 1];
+        items[size - 1] = null;
+        size -= 1;
+        return b;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size || index < 0){
+            return null;
+        }
+        else{
+            return items[index];
+        }
+    }
+}
--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -5,4 +5,48 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
 
+    @Test
+    public void isEmptyAndSizeTest() {
+        Deque<Character> ad1 = new ArrayDeque<>();
+
+        assertThat(ad1.isEmpty()).isTrue();
+        assertThat(ad1.size()).isEqualTo(0);
+        ad1.addLast('f');
+        assertThat(ad1.isEmpty()).isFalse();
+        assertThat(ad1.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void getTest() {
+        Deque<String> ad1 = new ArrayDeque<>();
+        ad1.addFirst("myTest");
+        assertWithMessage("get receives an invalid index").that(ad1.get(33)).isNull();
+        assertWithMessage("get receives an invalid index").that(ad1.get(-2)).isNull();
+        assertThat(ad1.get(0)).isNotEqualTo("myTest");
+    }
+
+    @Test
+    public void removeFirstAndLastTest() {
+        Deque<Integer> ad = new ArrayDeque<>();
+        ad.addLast(2);
+        ad.addFirst(1);
+        assertThat(ad.toList()).containsExactly(1, 2);
+        ad.removeLast();
+        assertThat(ad.toList()).containsExactly(1);
+        ad.removeFirst();
+        assertThat(ad.removeLast()).isNull();
+    }
+
+    @Test
+    public void resizeDownTest() {
+        Deque<Integer> ad1 = new ArrayDeque<>();
+        for (int i = 0; i <10000; i ++) {
+            ad1.addLast(0);
+        }
+        assertThat(ad1.size()).isEqualTo(10000);
+        for (int j = 0; j < 9999; j ++) {
+            ad1.removeFirst();
+        }
+        assertThat(ad1.size()).isLessThan(30);
+    }
 }
